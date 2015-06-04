a95c0506b2e5f53e2ffe925363783ad5e8505193
owuor91 <john.owuor@caytree.com>
2015-06-04 10:31:21 +0300

    Parsing JSON object and displaying in URL

---
+++ app/src/main/java/com/example/johno_000/json/MainActivity.java
@@ -0,0 +1,154 @@
+package com.example.johno_000.json;
+
+import android.os.AsyncTask;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+public class MainActivity extends ActionBarActivity {
+    public Spinner categories;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*categories = (Spinner) findViewById(R.id.categories);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(MainActivity.this, R.array.categories, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        categories.setAdapter(adapter);*/
+
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        final String url = "http://javatechig.com/api/get_category_posts/?dev=1&slug=android";
+        new AsyncHttpTask().execute(url);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    public class AsyncHttpTask extends AsyncTask<String,Void, Integer> {
+        public String TAG;
+        private String[] blogTitles;
+        @Override
+        protected Integer doInBackground(String... params) {
+            InputStream inputStream = null;
+            HttpURLConnection urlConnection=null;
+            Integer result = 0;
+            try{
+                URL url = new URL(params[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+
+                urlConnection.setRequestProperty("Cobtent-Type", "application/json");
+                urlConnection.setRequestProperty("Accept", "application/json");
+
+                urlConnection.setRequestMethod("GET");
+                int statusCode = urlConnection.getResponseCode();
+
+                if (statusCode ==200){
+                    inputStream = new BufferedInputStream(urlConnection.getInputStream());
+                    String response = convertInputStreamToString(inputStream);
+                    parseResult(response);
+                    result =1;
+                }
+                else {
+                    result = 0;
+                }
+
+            }
+            catch (Exception e){
+                Log.d(TAG, e.getLocalizedMessage());
+            }
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(Integer result) {
+            ListView listView = (ListView) findViewById(R.id.listView);
+            if (result==1){
+                ArrayAdapter arrayAdapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1,blogTitles);
+                listView.setAdapter(arrayAdapter);
+            }
+            else {
+                Log.e(TAG, "Failed to fetch data");
+            }
+        }
+
+        private String convertInputStreamToString(InputStream inputStream) throws IOException {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String line = "";
+            String result = "";
+            while ((line = bufferedReader.readLine()) != null){
+                result+=line;
+            }
+
+            if (null!=inputStream){
+                inputStream.close();
+            }
+            return result;
+        }
+
+        private void parseResult(String result){
+            try {
+                JSONObject response = new JSONObject(result);
+                JSONArray posts = response.optJSONArray("posts");
+                blogTitles = new String[posts.length()];
+
+                for (int i =0; i<posts.length(); i++){
+                    JSONObject post = posts.optJSONObject(i);
+                    String title = post.optString("title");
+                    blogTitles[i]=title;
+                }
+
+            }
+            catch (JSONException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
